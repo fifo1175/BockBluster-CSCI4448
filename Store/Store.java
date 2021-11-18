f19a2287af78c733f3b2f397e6aaf96e705a4dc3
Nick <nisp1104@colorado.edu>
2021-11-17 19:33:26 -0700

    Added GetMovie functionality, added libraries to wrap text output

--- Store/Store.java
+++ Store/Store.java
@@ -11,6 +11,8 @@ import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 
+import org.apache.commons.text.WordUtils;
+
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -149,9 +151,24 @@ public class Store {
                 .header("x-rapidapi-key", x_rapidapi_key)
                 .asJson();
 
+        org.json.JSONObject result = response.getBody().getObject();
 
+        String title = result.getString("Title");
+        String imdbID = result.getString("imdbRating");
+        String year = result.getString("Year");
+        String filmRating = result.getString("Rated");
+        String runtime = result.getString("Runtime");
+        String imdbRating = result.getString("imdbRating");
+        String genre = result.getString("Genre");
+        String plot = result.getString("Plot");
+        String director = result.getString("Director");
+        String actors = result.getString("Actors");
+        String country = result.getString("Country");
 
-        return null;
+        Movie resultMovie = new Movie(title, imdbID, year, filmRating, runtime, imdbRating, genre, plot, director,
+                actors, country);
+
+        return resultMovie;
     }
 
     public ArrayList<Movie> GenreSearch(String Genre) {  // search API for movies by genre, return 5 of that genre
@@ -266,21 +283,66 @@ public class Store {
 
             System.out.println("Select an option below:");
 
-            org.json.JSONArray results = new org.json.JSONArray();
+            org.json.JSONObject result = null;
+            org.json.JSONArray resultArray = new org.json.JSONArray();
             try {
-                results = searchResult.getBody().getObject().getJSONArray("Search");
+                resultArray = searchResult.getBody().getObject().getJSONArray("Search");
             } catch (JSONException e) {
                 System.out.println("\'" + movieTitle + "\' produced no results.");
             }
 
-            for (int i = 0; i < (results.length()) / 2; i++) {
+            for (int i = 0; i < (resultArray.length()) / 2; i++) {
                 //System.out.println(results.getJSONObject(1));
 
-                org.json.JSONObject result = results.getJSONObject(i);
+                result = resultArray.getJSONObject(i);
                 String title = result.getString("Title");
                 String year = result.getString("Year");
 
                 System.out.println(i + 1 + ": " + title + " (" + year + ")");
+            }
+
+            System.out.println("6: Return to Customer Home Page");
+
+            while(true) {
+                int searchChoice = scanner.nextInt();
+
+                switch (searchChoice) {
+                    case 1:
+                        result = resultArray.getJSONObject(0);
+                        break;
+                    case 2:
+                        result = resultArray.getJSONObject(1);
+                        break;
+                    case 3:
+                        result = resultArray.getJSONObject(2);
+                        break;
+                    case 4:
+                        result = resultArray.getJSONObject(3);
+                        break;
+                    case 5:
+                        result = resultArray.getJSONObject(4);
+                        break;
+                    case 6:
+                        return 1;
+                    default:
+                        System.out.println("Invalid input; please enter option 1-6");
+                        break;
+                }
+
+                Movie resultMovie = GetMovie(result.getString("imdbID"));
+
+                System.out.println("Title: " + resultMovie.title);
+                System.out.println("Release Year: " + resultMovie.year);
+                System.out.println("Film Rating: " + resultMovie.filmRating);
+                System.out.println("Runtime: " + resultMovie.runtime);
+                System.out.println("IMDb Rating: " + resultMovie.imdbRating);
+                System.out.println("Genre: " + resultMovie.genre);
+                System.out.println("Plot: " + WordUtils.wrap(resultMovie.plot, 90));
+                System.out.println("Director: " + resultMovie.director);
+                System.out.println("Actors: " + resultMovie.actors);
+                System.out.println("Country: " + resultMovie.country);
+
+                return 1;
             }
         }
         if (choice == 2) {
